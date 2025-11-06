b5370ce4dfcc1e91510cdd1affa690b9610f47d0
M0b1el <duartemarquesl68@gmail.com>
2025-11-06 13:33:47 -0300

    V0.0.3 | Inicio do Front

--- ServerWeb/main.js
+++ ServerWeb/main.js
@@ -1,1 +1,40 @@
 console.log("Script vinculado!")
+
+function configurarBotao(idBotao, nome, idEstadoTexto) {
+  let ligado = false;
+  const btn = document.getElementById(idBotao);
+  const estadoElemento = document.getElementById(idEstadoTexto);
+
+  btn.addEventListener("click", () => {
+    ligado = !ligado;
+
+    if (ligado) {
+      btn.textContent = `Desligar ${nome}`;
+      btn.classList.remove("off");
+      btn.classList.add("on");
+
+      estadoElemento.textContent = nome === "Regadores" ? "Ligados" : "Ligado";
+      estadoElemento.classList.remove("off");
+      estadoElemento.classList.add("on");
+
+      // fetch(`/ligar_${idBotao}`);
+    } else {
+      btn.textContent = `Ligar ${nome}`;
+      btn.classList.remove("on");
+      btn.classList.add("off");
+
+      estadoElemento.textContent =
+        nome === "Regadores" ? "Desligados" : "Desligado";
+      estadoElemento.classList.remove("on");
+      estadoElemento.classList.add("off");
+
+      // fetch(`/desligar_${idBotao}`);
+    }
+  });
+}
+
+// Aplicando aos 4 botões
+configurarBotao("btn-estufa", "Estufa", "state-estufa");
+configurarBotao("btn-luz", "Iluminação", "state-luz");
+configurarBotao("btn-fans", "Ventilação", "state-fans");
+configurarBotao("btn-regar", "Regadores", "state-regar");
